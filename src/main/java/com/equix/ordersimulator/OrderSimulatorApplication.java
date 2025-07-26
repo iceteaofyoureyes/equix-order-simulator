279c00a67dcc5322001718fcf52d95cee1f20068
hiepnh28 <hiepnh@tasc.vn>
2025-07-26 20:59:18 +0700

    chore(api docs): add swagger for api doc

--- src/main/java/com/equix/ordersimulator/OrderSimulatorApplication.java
+++ src/main/java/com/equix/ordersimulator/OrderSimulatorApplication.java
@@ -1,9 +1,18 @@
 package com.equix.ordersimulator;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Order Simulator API",
+                version = "1.0",
+                description = "Order Simulator Service API Docs"
+        )
+)
 public class OrderSimulatorApplication {
 
     public static void main(String[] args) {
